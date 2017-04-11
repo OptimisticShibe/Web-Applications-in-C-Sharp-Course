7c39e94fe89935bdfdae8d7c5ec013a35d851867
OptimisticShibe <britishdragons123@gmail.com>
2017-04-10 20:53:28 -0700

    2nd Class. Initial repository creation.

---
+++ HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloWorld.Models;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Products()
+        {
+            var products = new Product[]
+            {
+        new Product{ ProductId = 1, Name = "First One", Price = 1.11m, Count = 2},
+        new Product{ ProductId = 2, Name="Second One", Price = 2.22m, Count = 1},
+        new Product{ ProductId = 3, Name="Third One", Price = 3.33m, Count = 0},
+        new Product{ ProductId = 4, Name="Fourth One", Price = 4.44m, Count = 5},
+            };
+
+            return View(products);
+        }
+
+        public ActionResult Product()
+        {
+            var myProduct = new Product
+            {
+                ProductId = 1,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Category = "water-sports",
+                Price = 200m,
+            };
+
+            return View(myProduct);
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet] //Ask about
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost] //Ask about
+        public ActionResult RsvpForm(Models.GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid) // check on validation
+            {
+                return View("Thanks", guestResponse);
+            }
+            else // if false, stay on the same page
+            {
+                return View();
+            }
+        }
+    }
+}
